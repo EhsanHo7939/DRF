1392917b636dd70e1f0b2a82a5be9be600b3c802
Ehsan Ho <EhsanHo7939@gmail.com>
2021-11-27 09:41:32 +0330

    Add Customized Permissions & Modify Global and Specific Permissions.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser
+from .permissions import IsSuperUserOrStaffReadOnly_List, IsSuperUserOrStaffReadOnly_Detail, IsAuthorOrReadOnly, IsStaffOrReadOnly
 from blog.models import Article
 from django.contrib.auth.models import User
 from .serializers import ArticleSerializer, UserSerializer
@@ -15,15 +16,16 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly,)
 
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly_List,)
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminUser,)
+    permission_classes = (IsSuperUserOrStaffReadOnly_Detail,)
