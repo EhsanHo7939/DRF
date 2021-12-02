7fe8c7804e3e8ec64fc7e8c0455e632367f6bbf0
Ehsan Ho <EhsanHo7939@gmail.com>
2021-12-02 03:47:19 +0330

    Add RevokeToken View and Url Config

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import ArticleList, ArticleDetail, UserList, UserDetail
 from rest_framework.authtoken.views import obtain_auth_token
+from api.views import RevokeToken
 
 
 app_name = "api"
@@ -11,5 +12,6 @@ urlpatterns = [
     path("<int:pk>", ArticleDetail.as_view(), name="detail"),
     path("users/", UserList.as_view(), name="user_list"),
     path("users/<int:pk>", UserDetail.as_view(), name="user_detail"),
-    path('token-auth/', obtain_auth_token),
+    path("token-auth/", obtain_auth_token, name="obtain_token"),
+    path("token-revoke/", RevokeToken.as_view(), name="revoke_token"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .permissions import IsSuperUserOrStaffReadOnly_List, IsSuperUserOrStaffReadOnly_Detail, IsAuthorOrReadOnly, IsStaffOrReadOnly
 from blog.models import Article
 from django.contrib.auth.models import User
@@ -29,3 +31,11 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsSuperUserOrStaffReadOnly_Detail,)
+
+
+class RevokeToken(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request):
+        request.auth.delete()
+        return Response(status=204)
