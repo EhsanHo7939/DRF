3657d4e62e62cff28de403bb14247abdc08d26a2
Ehsan Ho <EhsanHo7939@gmail.com>
2021-12-04 19:29:53 +0330

    Install JWT Authentication and Url Config

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import ArticleList, ArticleDetail, UserList, UserDetail
 # from rest_framework.authtoken.views import obtain_auth_token
 # from api.views import RevokeToken
+from rest_framework_simplejwt import views as jwt_views
 
 
 app_name = "api"
@@ -14,6 +15,8 @@ urlpatterns = [
     path("users/<int:pk>", UserDetail.as_view(), name="user_detail"),
     path('auth/', include('dj_rest_auth.urls'), name="authentication"),
     path('auth/registration/', include('dj_rest_auth.registration.urls'), name="registration"),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     # path("token-auth/", obtain_auth_token, name="obtain_token"),
     # path("token-revoke/", RevokeToken.as_view(), name="revoke_token"),
 ]
