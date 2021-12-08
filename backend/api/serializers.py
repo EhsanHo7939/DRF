fd304c79fe7068e0b42fa66d18b3f9e6c7ac24b5
Ehsan Ho <EhsanHo7939@gmail.com>
2021-12-08 20:49:13 +0330

    Use Serializer Method Field for Nested Author Data

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,14 +3,23 @@ from blog.models import Article
 from django.contrib.auth import get_user_model
 
 
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ["id", "username", "first_name", "last_name"]
+# class AuthorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = get_user_model()
+#         fields = ["id", "username", "first_name", "last_name"]
 
 
 class ArticleSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer()
+    def get_author(self, obj):
+        return {
+            "username": obj.author.username,
+            "first_name": obj.author.first_name,
+            "last_name": obj.author.last_name,
+            "email": obj.author.email,
+        }
+
+    author = serializers.SerializerMethodField("get_author")
+
     class Meta:
         model = Article
         fields = "__all__"
